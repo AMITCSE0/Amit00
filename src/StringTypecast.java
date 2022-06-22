d4693c759b3c705300601947ee0402512add42ee
AMITCSE0 <amitkumar171717@gmail.com>
2022-06-22 20:36:00 +0530

    hii

---
+++ src/StringTypecast.java
@@ -0,0 +1,10 @@
+public class StringTypecast { static String checkConevrtion(int a)
+{
+    Integer i = a;
+    return i.toString();
+}
+    public static void main(String[] args) {
+        System.out.println(checkConevrtion(22));
+    }
+}
+
